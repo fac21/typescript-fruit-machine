9785084024a76c59cd3f76f8667b77fea941845d
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-20 16:43:04 +0100

    Win condition works
    Co-authored-by: chunzg <chunzhang53@gmail.com>

--- src/FruitMachine.tsx
+++ src/FruitMachine.tsx
@@ -8,6 +8,21 @@ export function FruitMachine() {
   const [slot2, setSlot2] = React.useState("");
   const [slot3, setSlot3] = React.useState("");
   const [slot4, setSlot4] = React.useState("");
+  const [winner, setWinner] = React.useState(false);
+  const [clicks, setClicks] = React.useState(0);
+
+  function checkForWin() {
+    if (slot1.length > 0) {
+      if (slot1 === slot2) {
+        // console.log("slot1 is the same as slot2", slot1, slot2);
+        if (slot2 === slot3) {
+          if (slot3 === slot4) {
+            setWinner(true);
+          }
+        }
+      }
+    }
+  }
 
   function getRandomColour(colours: Array<string>): string {
     const randIndex: number = Math.floor(Math.random() * colours.length);
@@ -15,16 +30,21 @@ export function FruitMachine() {
   }
 
   function handleClick() {
-    let colours: Array<string> = ["black", "red", "green", "blue"];
+    let colours: Array<string> = ["yellow", "red", "green", "blue"];
     setSlot1(getRandomColour(colours));
     setSlot2(getRandomColour(colours));
     setSlot3(getRandomColour(colours));
     setSlot4(getRandomColour(colours));
+    checkForWin();
+    setClicks(clicks + 1);
+    console.log(slot1, slot2, slot3, slot4, winner);
+    console.log("winner?", winner);
   }
 
-  // React.useEffect(() => {
+  React.useEffect(() => {
+    checkForWin();
+  }, [slot1, slot2, slot3, slot4]);
 
-  // }, [slot1, slot2, slot3, slot4]);
   return (
     <>
       <div className='slotContainer'>
@@ -33,7 +53,14 @@ export function FruitMachine() {
         <Slot slotColour={slot3} />
         <Slot slotColour={slot4} />
       </div>
-      <button onClick={handleClick}>Play!</button>
+
+      {clicks > 0 && !winner ? <p>Try again?</p> : ""}
+
+      {winner ? (
+        <p>You've won!!</p>
+      ) : (
+        <button onClick={handleClick}>Play!</button>
+      )}
     </>
   );
 }
